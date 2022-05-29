e1adfa9e6437ade492b561fdeaf583e305c86312
daniel aslan <daniel.aslan@outlook.com>
2022-05-29 21:24:06 +0100

    adding the main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+'''
+This a bible verse picker similiar to the daily bread.
+It will pick a bible verse at random and email (or text) it daily.
+I will also try to find a devotional that will give you a synposis.
+'''
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
